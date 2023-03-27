80dd6ce7d6df31d7184004e032c08fcf039e4032
Hsuan19971005 <g19971005@gmail.com>
2023-03-27 10:25:50 +0800

    Add VoteLog model. Add counter cache. Arrange codes.

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,9 +1,10 @@
 class CandidatesController < ApplicationController
+  before_action :find_candidate, only: [:show, :edit, :update, :destroy, :vote]
   def index
     @candidates = Candidate.all
   end
   def show
-    @candidate = Candidate.find_by(id: params[:id])
+    # @candidate = Candidate.find_by(id: params[:id])
   end
   def new
     @candidate = Candidate.new
@@ -12,41 +13,40 @@ class CandidatesController < ApplicationController
   def create
     @candidate = Candidate.new(candidate_params)
     if @candidate.save
-      flash[:notice] = "Add new candidate success!"
-      redirect_to '/candidates'
+      redirect_to '/candidates', notice: "Add new candidate success!"
     else
-      # redirect_to '/candidates/new'
       render :new
     end
   end
   def edit
-    @candidate = Candidate.find_by(id: params[:id])
+    # @candidate = Candidate.find_by(id: params[:id])
   end
   def update
-    @candidate = Candidate.find_by(id: params[:id])
+    # @candidate = Candidate.find_by(id: params[:id])
     if @candidate.update(candidate_params)
-      flash[:notice] = "Update success!"
-      redirect_to '/candidates'
+      redirect_to '/candidates', notice:"Update success!"
     else  
       render :edit
     end
   end
   def destroy
-    @candidate = Candidate.find_by(id: params[:id])
+    # @candidate = Candidate.find_by(id: params[:id])
     @candidate.destroy
-    flash[:notice] = "You destroy the data!"
-    redirect_to "/candidates"
+    redirect_to "/candidates", notice: "You destroy the data!"
   end
   def vote
-    @candidate = Candidate.find_by(id: params[:id])
-    @candidate.increment(:votes)
-    @candidate.save
-    flash[:notice] = "You voted."
-    redirect_to "/candidates"
+    # @candidate = Candidate.find_by(id: params[:id])
+    # VoteLog.create(candidate: @candidate, ip_address: request.remote_ip)
+    @candidate.vote_logs.create(ip_address: request.remote_ip)
+
+    redirect_to "/candidates",notice:"You voted."
   end
 
   private
   def candidate_params
     params.require(:candidate).permit(:name, :party, :age, :politics)
+  end
+  def find_candidate
+    @candidate = Candidate.find_by(id: params[:id])
   end
 end
