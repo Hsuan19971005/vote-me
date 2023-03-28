0a6fb422d0dd766ae3dfb6accc6ea7c9ea256829
Hsuan19971005 <g19971005@gmail.com>
2023-03-28 20:10:04 +0800

    Add search function.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    rescue_from ActiveRecord::RecordNotFound, with: :id_not_found
+    def id_not_found
+        # render html: "Fail!!!"
+        render file:Rails.root.join('public', '404.html'), layout: false, status: 404 and return 
+    end
 end
--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,10 +2,14 @@ class CandidatesController < ApplicationController
   before_action :find_candidate, only: [:show, :edit, :update, :destroy, :vote]
   def index
     @candidates = Candidate.all
+    if params[:keyword].present?
+      @candidates = @candidates.where("name like ?", "%#{params[:keyword]}%").or(@candidates.where("party like ?", "%#{params[:keyword]}%"))
+    end
   end
+  
   def show
-    # @candidate = Candidate.find_by(id: params[:id])
   end
+  
   def new
     @candidate = Candidate.new
   end
@@ -18,24 +22,24 @@ class CandidatesController < ApplicationController
       render :new
     end
   end
+
   def edit
-    # @candidate = Candidate.find_by(id: params[:id])
   end
+  
   def update
-    # @candidate = Candidate.find_by(id: params[:id])
     if @candidate.update(candidate_params)
       redirect_to '/candidates', notice:"Update success!"
     else  
       render :edit
     end
   end
+  
   def destroy
-    # @candidate = Candidate.find_by(id: params[:id])
     @candidate.destroy
     redirect_to "/candidates", notice: "You destroy the data!"
   end
+
   def vote
-    # @candidate = Candidate.find_by(id: params[:id])
     # VoteLog.create(candidate: @candidate, ip_address: request.remote_ip)
     @candidate.vote_logs.create(ip_address: request.remote_ip)
 
@@ -47,6 +51,6 @@ class CandidatesController < ApplicationController
     params.require(:candidate).permit(:name, :party, :age, :politics)
   end
   def find_candidate
-    @candidate = Candidate.find_by(id: params[:id])
+    @candidate = Candidate.find(params[:id])
   end
 end
